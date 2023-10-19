beb42ac98e2526658cbe9477336567ddf0235716
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-10-19 16:04:43 +0900

    04

--- main.c
+++ main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int inc(int counter);
-int main(int argc, char *argv[])
-{
-    int i = 10 ;
-    printf("함수 호출 전 i= : %d\n ", i);
-    inc(i);
-    //i = inc(i); 함수 실행 후 값을 나타내고 싶으면  
-    printf("함수 호출 후 i = %d\n", i); 
+void f(void);
+//int i ; 
+
+int main(void)
+{ 
+    int i ;
+    for(i=0 ; i<5; i++)
+    {
+            f();
+     }
     
   system("PAUSE");	
   return 0;
 }
-//함수에 쓰이는 값은 복사본으로 들어감 call by value
 
-int inc(int counter)
+void f(void)
 {
-    counter++;
-    return counter ;
-}
+     int i ;
+     for (i=0; i<10 ; i++)
+     printf("#");
+ }
