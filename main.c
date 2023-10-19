90cb5f43db9391eebb7a8e1e0152ba50c62768fd
s25y25n <144671761+s25y25n@users.noreply.github.com>
2023-10-19 16:11:17 +0900

    05

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void f(void);
-//int i ; 
+void sub(void);
+
 
 int main(void)
 { 
     int i ;
-    for(i=0 ; i<5; i++)
+    for (i = 0 ; i <3 ; i++) 
     {
-            f();
-     }
-    
+        sub();
+    }
+ 
   system("PAUSE");	
   return 0;
 }
 
-void f(void)
+void sub(void) 
 {
-     int i ;
-     for (i=0; i<10 ; i++)
-     printf("#");
- }
+     int auto_count = 0;
+     static int static_count = 0;
+     auto_count++;
+     static_count++;
+     printf("auto_count = %d\n", auto_count);
+     printf("static_count = %d\n", static_count);
+     
+}
